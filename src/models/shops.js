b7575f97057a3cbfbb9c1468422aa28b5516b120
atharmohammad <mohd.rule123@gmail.com>
2021-09-20 12:13:18 +0530

    auth routes

--- src/models/shops.js
+++ src/models/shops.js
@@ -23,8 +23,56 @@ const schema = new mongoose.Schema(
             trim: true,
             required:true
         },
+        Token:{
+            type:String,
+        }
     }
 )
+
+schema.methods.toJSON = function(){ //this toJSON is a special function
+    //whenever any user object is send on request it will transfrom to JSON.stringify()
+    //this toJSON decides what to give back to stringify
+  const shop = this;
+  const shopObject = shop.toObject();
+
+  delete shopObject.Password;
+  delete shopObject.Token;
+
+  return shopObject;
+}
+
+schema.methods.generateToken = async function(){
+    const shop = this;
+    const token = jwt.sign({_id : shop._id.toString()},process.env.JWT_SECRET);
+    shop.Token = token
+    await shop.save();
+    console.log(token)
+    return token;
+}
+
+schema.statics.findByCredentials = async function(ShopName,Password){
+    const shop = await this.model('Shop').findOne({ShopName:ShopName});
+    if(!shop){
+      throw new Error ('store not found!');
+    }
+  
+    const isMatch = await bcrypt.compare(Password,shop.Password);
+  
+    if(!isMatch){
+      throw new Error('Store Name and password not matched');
+    }
+  
+    return shop
+  
+  }
+
+schema.pre('save',async function(next){
+    const shop = this;
+    if(shop.isModified('Password')){
+      shop.password = await bcrypt.hash(shop.password,8);
+    }
+    next();
+})
 
 const table = mongoose.model('Shop',schema);
 module.exports = table;
---
+++ src/routers/authentication.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const router = new express.Router();
+const Shop = require('../models/');
+const auth = require("../middlewares/Auth");
+
+router.post('/login',async(req,res,next)=>{
+    try{
+        const ShopName = req.body.ShopName;
+        const Password = req.body.Password;
+
+        const shop = await Shop.findByCredentials(ShopName,Password);
+        await shop.generateToken()
+
+        return res.status(200).send(shop)
+    }catch(e){
+        console.log(e);
+        return res.status(400).send(e);
+    }
+});
+
+router.get('/logout',auth,async(req,res,next)=>{
+    try{
+        req.shop.Token = null;
+        await res.shop.save();
+        return res.status(200).send();
+    }catch(e){
+        return res.status(400).send();
+    }
+});
